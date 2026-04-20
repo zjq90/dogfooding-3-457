bea134f5488d8eba43dd5f54132c6ff85e56f037
zjq90 <1066455598@qq.com>
2026-04-20 11:21:07 +0800

    Delete oa-microservices directory

--- oa-microservices/employee-service/src/main/java/com/oa/employee/mapper/DepartmentMapper.java
+++
@@ -1,7 +0,0 @@
-package com.oa.employee.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.oa.employee.entity.Department;
-
-public interface DepartmentMapper extends BaseMapper<Department> {
-}
